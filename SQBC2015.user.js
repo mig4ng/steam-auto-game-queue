3e19c895640ce1a8b6f17ab9ea5df5e41b0176f5
mig4ng <mig4ng@gmail.com>
2015-12-24 04:49:17 +0000

    added time between refreshing
    from half a second to 6 seconds, you can remove it by making maxTime and minTime 0

--- SQBC2015.user.js
+++ SQBC2015.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Steam QueueBot Christmas 2015
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Reminds you to check if you have cards to obtain by checking the queue steam arranged for you and auto completes it for you.
 // @author       mig4ng
 // @require      http://code.jquery.com/jquery-latest.js
@@ -13,12 +13,14 @@
 
 var alerts = true; // change to false if you don't want the alerts
 var donateAlerts = true; // change to false if you don't want the annoying tab to open everytime, btw consider donating a card or a cheap skin if you found this script somewhat useful
+var maxTime = 6000; // max time in miliseconds between the load of the page and the button pressing
+var minTime = 500; // min time in miliseconds between the load of the page and the button pressing
 
 if ($('span.queue_sub_text').length) {
-    $J('#next_in_queue_form').submit();
+    setTimeout(function(){ $J('#next_in_queue_form').submit(); }, (maxTime-minTime)*Math.random() + minTime);
 }
 if ($('span.finish_queue_text').length) {
-    $J('#next_in_queue_form').submit();
+    setTimeout(function(){ $J('#next_in_queue_form').submit(); }, (maxTime-minTime)*Math.random() + minTime);
 }
 if ($('div.discover_queue_empty_refresh_btn').length) {
     if(donateAlerts){
